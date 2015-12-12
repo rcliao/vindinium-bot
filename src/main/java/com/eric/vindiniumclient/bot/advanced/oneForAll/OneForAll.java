e726573af5b53ef63c193fbff3f13c6cdbadb6dd
Eric Liao <rcliao01@gmail.com>
2015-12-11 22:29:49 -0800

    increase aggression on mine than tavern

--- src/main/java/com/eric/vindiniumclient/bot/advanced/oneForAll/OneForAll.java
+++ src/main/java/com/eric/vindiniumclient/bot/advanced/oneForAll/OneForAll.java
@@ -84,7 +84,7 @@ public class OneForAll implements AdvancedBot {
                         (
                             (gameState.getMe().getLife() < 60.0) ?
                                 ((70.0 - gameState.getMe().getLife()) / 100.0) :
-                                (gameState.getMe().getMineCount() / (gameState.getMines().size() / 2)) * 0.4
+                                (gameState.getMe().getMineCount() / (gameState.getMines().size() / 2)) * 0.3
                         );
                 diffuseMap(gameState, valueMap, Sets.newHashSet(), v, value, 30);
             });
